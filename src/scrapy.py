c95548b7bda906c4295e74ad492895795b609453
KIRIDHARAN S <kiridharan217dec2001@gmail.com>
2021-05-04 20:08:01 +0530

    added bike json

--- src/scrapy.py
+++ src/scrapy.py
@@ -13,12 +13,11 @@ list_links = [
             "https://autoportal.com/newbikes/bike-finder/1:1/1:2/1:3/1:4/1:5/1:6/1:7/1:8/1:9/1:10/1:11/1:12/1:13/1:14/1:15/1:16/1:17/1:18/1:19/1:20/1:21/1:26/1:34/page/6/50/popularity/desc/",
             "https://autoportal.com/newbikes/bike-finder/1:1/1:2/1:3/1:4/1:5/1:6/1:7/1:8/1:9/1:10/1:11/1:12/1:13/1:14/1:15/1:16/1:17/1:18/1:19/1:20/1:21/1:26/1:34/page/7/50/popularity/desc/",
 ]
-base_url = "https://autoportal.com/newbikes/bike-finder/1:1/1:2/1:3/1:4/1:5/1:6/1:7/1:8/page/1/50/popularity/desc/"
 data = {}
 lis = []
 def get_data(url):
     base_link_address = "https://autoportal.com"
-    r = requests.get(base_url)
+    r = requests.get(url)
     content = r.content
     soup = BeautifulSoup(content , features="html.parser")
     bikes = soup.find_all('div', class_='item')
@@ -33,41 +32,31 @@ def get_data(url):
             img = bike.img['data-original']
             details_link = bike.img['data-link']
             bike_detail_link = base_link_address + details_link
-            
+            variant_available = bike.find('span',class_='count-in').text
             data = {
-                
-                    "bike_name": bike_name.strip(),
-                    "bike_detail name": bike_detail_name,
-                    "bike_price": bike_price,
-                    "bike_engine": engine,
-                    "bike_mileage": mileage,
-                    "bike_power": power,
-                    "bike_img_url": img,
-                    "detail_page_url": bike_detail_link,
-               
+                "bike_name": bike_name.replace('\n','').replace(' ',''),
+                "bike_detail_name": bike_detail_name.replace('\n','').replace(' ',''),
+                "bike_price": bike_price.replace(' ',''),
+                "engine": engine.replace(' ',''),
+                "mileage": mileage.replace(' ',''),
+                "power": power.replace(' ',''),
+                "img": img,
+                "bike_detail_link": bike_detail_link.replace(' ',''),
+                # "variant_available":variant_available,  
             }
-            # bike_list=[]
-            # bike_list.append(bike_name.strip())
-            # bike_list.append(bike_detail_name.strip())
-            # bike_list.append(bike_price.strip())
-            # bike_list.append(engine.strip())
-            # bike_list.append(mileage.strip())
-            # bike_list.append(power.strip())
-            # bike_list.append(img.strip())
-            
-            # lis.append(bike_list)
-            
 
-
+            lis.append(data)
+            print("Your data is been converting to json with list index:",print(len(lis)))
+            with open('bike.json', 'w') as outfile:
+                json.dump(lis, outfile)
         except AttributeError:
             pass
+        
+
     
 
-def list_url(url_list):
-    for url in url_list:
-        print(url)
 
 
 if __name__ == '__main__':
-    get_data(base_url)
-    list_url(list_links)
+    for i in range(0,len(list_links)):
+        get_data(list_links[i])
